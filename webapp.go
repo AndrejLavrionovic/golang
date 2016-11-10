90c32b4c6d8d4f7f3418b6515e34d1ddd6555203
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-11-10 18:39:01 +0000

    Project run

--- webapp.go
+++ webapp.go
@@ -1,2 +1,15 @@
 package GolangTest
 
+import(
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main(){
+	http.Handle("/", handler)
+	http.ListenAndServe(":9999", nil)
+}
